747cf9522c8e30956024e8d0aa6dbcba68b22061
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-03-07 17:12:53 +0100

    chore(flake): install pnpm with the right node version

--- .flake/flake.nix
+++ .flake/flake.nix
@@ -2,19 +2,25 @@
 {
   description = "Dependencies";
 
-  inputs.nixpkgs_old.url = "github:NixOS/nixpkgs/nixos-23.05";
+  # inputs.nixpkgs_old.url = "github:NixOS/nixpkgs/nixos-23.05";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, nixpkgs_unstable, nixpkgs_old, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs_unstable,
+    # nixpkgs_old,
+    flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = nixpkgs.legacyPackages.${system}.extend overlays;
         unstable = nixpkgs_unstable.legacyPackages.${system};
-        old = (import nixpkgs_old {
-          inherit system;
-          config = { permittedInsecurePackages = [ "nodejs-16.20.2" ]; };
+        # old = (import nixpkgs_old {
+        #   inherit system;
+        #   config = { permittedInsecurePackages = [ "nodejs-16.20.2" ]; };
+        # });
+        overlays = (final: prev: {
+          nodePackages =
+            prev.nodePackages.override { nodejs = pkgs.nodejs_20; };
         });
         my-python-packages = python-packages:
           with python-packages;
@@ -32,6 +38,7 @@
           just # Simple make replacement https://just.systems/
           # old.nodejs_16 # node used for husky installation https://nodejs.org/en/
           nodejs_20
+          nodePackages.pnpm
           unstable.nushell # Nu Shell https://www.nushell.sh/
           python-with-my-packages
         ];
